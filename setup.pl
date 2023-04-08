7cbcd7bd2b3495830b368167770a5e5911ea6093
Daniel De Lellis <dan.delellis@gmail.com>
2023-04-08 01:32:29 -0700

    compartmentalize functions

--- setup.pl
+++ setup.pl
@@ -3,26 +3,51 @@ use Getopt::Long;
 use Data::Dumper;
 use Storable qw ( thaw  );
 use MIME::Base64;
-
-`userdel -r openttd`;
-
 #makes it easier to determine relative paths
 my $gitroot = `git rev-parse --show-toplevel`; chomp $gitroot;
 
-my $x=`$gitroot/create_user.pl`;
-if ($rv) {
-    die "Failed to execute user creation script: $!\n";
+#hashrefs for returned data from scripts
+my ($user_data,$unpack_data);
+
+
+cleanup_old();
+
+$user_data = setup_user();
+$unpack_data = download_unpack();
+generate_system_conf();
+
+
+finish();
+
+sub cleanup_old {
+    `userdel -r openttd 2> /dev/null`;
 }
-my $user_data =  thaw (decode_base64 $x ) ;
-my $cmd = "$user_data->{home_directory}/download_unpack.pl";
 
-my @y = `su $user_data->{username} -c "$cmd"`;
-if ($rv) {
-    die "Failed to run '$cmd': $!\n";
+sub setup_user {
+    my $x=`$gitroot/create_user.pl`;
+    if ($rv) {
+        die "Failed to execute user creation script: $!\n";
+    }
+    $x =  thaw (decode_base64 $x ) ;
+    return $x;
 }
 
-print "Setup Complete!\n";
-print "Unpacked to: $user_data->{home_directory}\n";
-print "UserName: $user_data->{username}\n";
-print "Password: \'$user_data->{password}\'\n";
+sub download_unpack {
+    my $cmd = "$user_data->{home_directory}/download_unpack.pl";
 
+    my $y = `su $user_data->{username} -c "$cmd"`;
+    if ($rv) {
+        die "Failed to run '$cmd': $!\n";
+    }
+    $y = thaw (decode_base64 $y);
+    return $y;
+}
+
+
+
+sub finish {
+    print "Setup Complete!\n";
+    print "Unpacked to: $user_data->{home_directory}\n";
+    print "Created User: $user_data->{username}\n";
+    print "Password: \'$user_data->{password}\'\n";
+}
