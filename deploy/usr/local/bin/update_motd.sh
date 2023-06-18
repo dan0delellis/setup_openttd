b877fc9ff41f8c1abe220be11503fbb001201def
Daniel De Lellis <dan.delellis@gmail.com>
2023-06-18 17:36:22 +0000

    i hate bash printf so much

--- deploy/usr/local/bin/update_motd.sh
+++ deploy/usr/local/bin/update_motd.sh
@@ -2,7 +2,6 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 eval "$(systemctl show --no-pager openttd-dedicated.service | grep WorkingDirectory)"
 motd="$WorkingDirectory/scripts/on_server_connect.scr"
-
 remaining=$(echo $(date -d "next sunday 03:30" +%s) - $(date +%s) | bc)
 days=$(echo $(echo $remaining / 86400 | bc) % 7 | bc)
 hours=$(echo "( $remaining % 86400 )" / 3600 | bc)
@@ -10,6 +9,7 @@ minutes=$(echo "( $remaining % 3600 )" / 60 | bc)
 seconds="00";
 msg=
 if [ $days != 0 ]; then
-    msg="$days days, "
+    msg="$days days,"
 fi
-printf  "say \"%s%02d:%02d:%02d until server reset\"\n" $msg $hours $minutes $seconds > $motd;
+printf  'say "%s %02d:%02d:%02d until server reset"\n' "$msg" "$hours" "$minutes" "$seconds" > $motd
+
